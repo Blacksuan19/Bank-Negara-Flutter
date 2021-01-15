b5628b2a3eec5ff64b0a976a1f4d60b080cb3fd0
blacksuan19 <i@blacksuan19.me>
2021-01-16 03:55:04 +0800

    initial Application
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// custom text style to match the website theme
+class TextStyles {
+  static TextStyle headline1(context) {
+    return Theme.of(context).textTheme.headline1.copyWith(color: Colors.red);
+  }
+
+  static TextStyle headline2(context) {
+    return Theme.of(context).textTheme.headline2.copyWith(color: Colors.red);
+  }
+
+  static TextStyle headline3(context) {
+    return Theme.of(context).textTheme.headline3.copyWith(color: Colors.red);
+  }
+
+  static TextStyle headline4(context) {
+    return Theme.of(context).textTheme.headline4.copyWith(color: Colors.red);
+  }
+
+  static TextStyle headline5(context) {
+    return Theme.of(context).textTheme.headline5.copyWith(color: Colors.red);
+  }
+
+  static TextStyle headline6(context) {
+    return Theme.of(context).textTheme.headline6.copyWith(color: Colors.red);
+  }
+}
